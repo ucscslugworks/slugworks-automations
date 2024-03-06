8617335426955ff6803f04f6ba0c8149900578b1
Ishan Madan <19366470+ishanm0@users.noreply.github.com>
2024-03-05 16:47:00 -0800

    better sample code

--- reader.py
+++ reader.py
@@ -1,10 +1,14 @@
-import RPi.GPIO as GPIO
+from time import sleep
+import sys
 from mfrc522 import SimpleMFRC522
-
 reader = SimpleMFRC522()
+
 try:
+    while True:
+        print("Hold a tag near the reader")
         id, text = reader.read()
-        print(id)
-        print(text)
-finally:
-        GPIO.cleanup()
+        print("ID: %s\nText: %s" % (id,text))
+        sleep(5)
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    raise
