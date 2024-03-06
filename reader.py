a0f6e0b782db63658767e21a964447b1259a4d07
Ishan Madan <19366470+ishanm0@users.noreply.github.com>
2024-03-05 17:24:49 -0800

    added timestamp checking

--- reader.py
+++ reader.py
@@ -1,19 +1,26 @@
 import sys
-from time import sleep
+from time import sleep, time
 
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 
 reader = SimpleMFRC522()
 
+timestamps = {}
+
+
 def read_card():
     try:
         id, _ = reader.read()
-        return hex(id)[2:]
+        id = hex(id)[2:-2]
+        if id in timestamps and time() - timestamps[id] < 5:
+            return False
+        timestamps[id] = time()
     except KeyboardInterrupt:
         raise
     except:
         return None
+
 
 try:
     while True:
