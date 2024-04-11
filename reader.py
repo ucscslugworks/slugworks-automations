77285efe046973710eb6738bdc8d840cc675a740
Ishan Madan <19366470+ishanm0@users.noreply.github.com>
2024-04-11 00:53:45 -0700

    debugging

--- reader.py
+++ reader.py
@@ -82,6 +82,7 @@ if __name__ == "__main__":
             card_id = nfc.read_card_queue_timeout(10)
             print(card_id)
             if card_id and card_id != last_id:
+                print(card_id, last_id)
                 last_id = card_id
                 response = sheet.scan_uid(card_id)
                 if not response:
