ef397fb6fa1c37b8d266eabf2e6ca44c0de9fbf3
Ishan Madan <19366470+ishanm0@users.noreply.github.com>
2024-04-28 20:54:11 -0700

    reader must be sudo

--- reader.sh
+++ reader.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+if [[ "$EUID" != 0 ]]; then
+    echo "(1) not root"
+    sudo -k # make sure to ask for password on next sudo ✱
+    if sudo true; then
+        echo "(2) correct password"
+    else
+        echo "(3) wrong password"
+        exit 1
+    fi
+fi
 source /home/slugworks/slugworks-access-cards/venv/bin/activate
-python3 /home/slugworks/slugworks-access-cards/reader.py
+sudo python3 /home/slugworks/slugworks-access-cards/reader.py
