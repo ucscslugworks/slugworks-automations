2e09c97a973dc0048846865d323fbb80642a6037
cedric <43191516+requiem85@users.noreply.github.com>
2024-05-19 11:05:26 -0700

    added functions

--- ms.py
+++ ms.py
@@ -1,48 +1,56 @@
 import sheet
 from ms_active_directory import *
 
-print("starting ....")
-domain = ADDomain("au.ucsc.edu")
-print("domain connected")
+def login():
+    print("starting ....")
+    domain = ADDomain("au.ucsc.edu")
+    print("domain connected")
+    session = domain.create_session_as_user("username", "password")
+    print("session created")
+    return session
 
-session = domain.create_session_as_user("admin.chartier@au.ucsc.edu", "Power2theAdmin!")
-print("session created")
-desired_attrs = ["mail"]
-single_group_member_list = session.find_members_of_group("au-slugworks-access", ["cn"])
-print("group members found")
 
-in_group = []
-for user in single_group_member_list:
 
-    # Access the attributes dictionary and get the value after 'cn'
-    attributes = user.all_attributes
-    cn_value = attributes["cn"]
-    print(cn_value)
-    in_group.append(cn_value)
+def add_user(session, to_add):
+    session.add_users_to_groups(list(to_add), ["au-slugworks-access"])
+    print("added users")
 
-sheet.get_sheet_data(False)
-print("checking sheet data")
-All = sheet.student_data.loc[
-    sheet.student_data["3D Printing"].isin(["Access", "Override Access"])
-]["CruzID"].values.tolist()
-staff = sheet.staff_data["CruzID"].values.tolist()
-print("sheet data checked")
 
-All = All + staff
-print("in_group", in_group)
-print("all", All)
 
-to_add = set(All) - set(in_group)
-print("unadded", to_add)
-to_remove = set(in_group) - set(All)
-print("to_remove", to_remove)
-print("math calculated")
 
-print("adding students")
-# for student in Unadded:
-session.add_users_to_groups(list(to_add), ["au-slugworks-access"])
+def remove_user(session, to_remove):
+    session.remove_users_from_groups(list(to_remove), ["au-slugworks-access"])
+    print("removed users")
 
-print("removing students")
-# to_remove_nonstaff= set(to_remove) - set(staff)
-# for student in to_remove:
-session.remove_users_from_groups(list(to_remove), ["au-slugworks-access"])
+def get_group(session):
+    single_group_member_list = session.find_members_of_group("au-slugworks-access", ["cn"])
+    print("group members found")
+
+    in_group = []
+    for user in single_group_member_list:
+
+        # Access the attributes dictionary and get the value after 'cn'
+        attributes = user.all_attributes
+        cn_value = attributes["cn"]
+        print(cn_value)
+        in_group.append(cn_value)
+
+    sheet.get_sheet_data(False)
+    print("checking sheet data")
+    All = sheet.student_data.loc[
+        sheet.student_data["3D Printing"].isin(["Access", "Override Access"])
+    ]["CruzID"].values.tolist()
+    staff = sheet.staff_data["CruzID"].values.tolist()
+    All = All + staff
+    to_add = set(All) - set(in_group)
+    to_remove = set(in_group) - set(All)
+    return to_add, to_remove
+
+
+
+def main():
+    session = login()
+    to_add, to_remove = get_group(session)
+    add_user(session, to_add)
+    remove_user(session, to_remove)
+    print("done")
