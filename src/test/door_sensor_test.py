96b5c5ad1f96ae890caf9bc1593f6448432dc115
Ishan Madan <19366470+ishanm0@users.noreply.github.com>
2024-07-14 00:17:56 -0700

    door sensor test

---
+++ src/test/door_sensor_test.py
@@ -0,0 +1,18 @@
+try:
+    import RPi.GPIO as GPIO  # type: ignore
+except RuntimeError:
+    print(
+        "Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script"
+    )
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+old_input_state = False
+
+while True:
+    input_state = GPIO.input(16)
+    if input_state != old_input_state:
+        print(input_state)
+        old_input_state = input_state
